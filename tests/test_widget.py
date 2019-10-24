da59be627e52c3c703ce31f430c98f199c15ce65
Juan Luis Cano Rodríguez <hello@juanlu.space>
2019-10-24 12:17:19 +0200

    Add widget tests

---
+++ tests/test_widget.py
@@ -0,0 +1,22 @@
+import pytest
+
+from czml3.widget import CZMLWidget
+
+
+def test_no_input_makes_empty_document():
+    widget = CZMLWidget()
+
+    assert len(widget.document.packets) == 1
+
+
+@pytest.mark.parametrize("cesium_version", ["1.62", "1.99"])
+def test_version(cesium_version):
+    widget = CZMLWidget(cesium_version=cesium_version)
+
+    assert cesium_version in widget.build_script()
+
+
+def test_to_html_contains_script():
+    widget = CZMLWidget()
+
+    assert widget.build_script() in widget.to_html()
