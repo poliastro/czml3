e5149f960241646857260980c98f5784958eda6f
Juan Luis Cano Rodríguez <hello@juanlu.space>
2019-10-24 12:38:31 +0200

    Add test for missing czml3.utils functionality

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,5 @@
+import pytest
+
 from czml3.properties import Color
 from czml3.utils import get_color
 
@@ -12,3 +14,10 @@ def test_get_color():
     assert get_color(0xFFCC00FF).rgba.values == expected_color.rgba.values
     assert get_color([255, 204, 0]).rgba.values == expected_color.rgba.values
     assert get_color([255, 204, 0, 255]).rgba.values == expected_color.rgba.values
+
+
+@pytest.mark.xfail
+def test_get_color_missing():
+    expected_color = Color(rgbaf=[1.0, 0.8, 0, 1.0])
+
+    assert get_color([1.0, 0.8, 0, 1.0]).rgbaf.values == expected_color.rgbaf.values
