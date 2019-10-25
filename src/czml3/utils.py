801e6c929af0cc30f891a53f2a26f35660b2827f
Juan Luis Cano Rodríguez <hello@juanlu.space>
2019-10-25 17:02:50 +0200

    Fix float color creation

--- src/czml3/utils.py
+++ src/czml3/utils.py
@@ -6,27 +6,38 @@ def get_color(color):
         return get_color(int(color.rsplit("#")[-1], 16))
     elif isinstance(color, int):
         if color > 0xFFFFFF:
-            values = [
-                (color & 0xFF000000) >> 24,
-                (color & 0x00FF0000) >> 16,
-                (color & 0x0000FF00) >> 8,
-                (color & 0x000000FF) >> 0,
-            ]
+            values = {
+                "rgba": [
+                    (color & 0xFF000000) >> 24,
+                    (color & 0x00FF0000) >> 16,
+                    (color & 0x0000FF00) >> 8,
+                    (color & 0x000000FF) >> 0,
+                ]
+            }
         else:
-            values = [
-                (color & 0xFF0000) >> 16,
-                (color & 0x00FF00) >> 8,
-                (color & 0x0000FF) >> 0,
-                0xFF,
-            ]
-    elif isinstance(color, list):
+            values = {
+                "rgba": [
+                    (color & 0xFF0000) >> 16,
+                    (color & 0x00FF00) >> 8,
+                    (color & 0x0000FF) >> 0,
+                    0xFF,
+                ]
+            }
+    elif isinstance(color, list) and all(isinstance(v, int) for v in color):
         if len(color) == 3:
-            values = color + [255]
+            values = {"rgba": color + [255]}
         elif len(color) == 4:
-            values = color[:]
+            values = {"rgba": color[:]}
+        else:
+            raise ValueError("Invalid number of values")
+    elif isinstance(color, list) and all(isinstance(v, float) for v in color):
+        if len(color) == 3:
+            values = {"rgbaf": color + [1.0]}
+        elif len(color) == 4:
+            values = {"rgbaf": color[:]}
         else:
             raise ValueError("Invalid number of values")
     else:
         raise ValueError("Invalid input")
 
-    return Color(rgba=values)
+    return Color(**values)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,7 +4,7 @@ from czml3.properties import Color
 from czml3.utils import get_color
 
 
-def test_get_color():
+def test_get_color_rgba():
     expected_color = Color(rgba=[255, 204, 0, 255])
 
     # TODO: Simplify after https://github.com/poliastro/czml3/issues/36
@@ -16,11 +16,12 @@ def test_get_color():
     assert get_color([255, 204, 0, 255]).rgba.values == expected_color.rgba.values
 
 
-@pytest.mark.xfail
-def test_get_color_missing():
-    expected_color = Color(rgbaf=[1.0, 0.8, 0, 1.0])
+def test_get_color_rgbaf():
+    expected_color = Color(rgbaf=[1.0, 0.8, 0.0, 1.0])
 
-    assert get_color([1.0, 0.8, 0, 1.0]).rgbaf.values == expected_color.rgbaf.values
+    # TODO: Simplify after https://github.com/poliastro/czml3/issues/36
+    assert get_color([1.0, 0.8, 0.0]).rgbaf.values == expected_color.rgbaf.values
+    assert get_color([1.0, 0.8, 0.0, 1.0]).rgbaf.values == expected_color.rgbaf.values
 
 
 @pytest.mark.parametrize("input", ["a", [0, 0, 0, 0, 0], [1.0, 1.0]])
