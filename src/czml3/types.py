1a5bb54ee642747ea4382b93e21af06b836b9850
Juan Luis Cano Rodríguez <hello@juanlu.space>
2019-11-06 12:11:05 +0100

    Style

--- src/czml3/types.py
+++ src/czml3/types.py
@@ -100,14 +100,14 @@ class RgbafValue(BaseCZMLObject):
 @attr.s(repr=False, frozen=True, kw_only=True)
 class RgbaValue(BaseCZMLObject):
     """A color specified as an array of color components [Red, Green, Blue, Alpha]
-     where each component is in the range 0-255. If the array has four elements,
-     the color is constant.
+    where each component is in the range 0-255. If the array has four elements,
+    the color is constant.
 
-      If it has five or more elements, they are time-tagged samples arranged as
-     [Time, Red, Green, Blue, Alpha, Time, Red, Green, Blue, Alpha, ...], where Time
-     is an ISO 8601 date and time string or seconds since epoch.
+    If it has five or more elements, they are time-tagged samples arranged as
+    [Time, Red, Green, Blue, Alpha, Time, Red, Green, Blue, Alpha, ...], where Time
+    is an ISO 8601 date and time string or seconds since epoch.
 
-     """
+    """
 
     values = attr.ib()
 
