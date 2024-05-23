413d9f2379b74af1eadf4e4bb58257d11d65ebf6
Daniel Stoops <danielst@omnisys.co.il>
2024-05-23 10:33:44 +0300

    Build from pyproject.toml (remove setup.cfg)

--- src/czml3/__init__.py
+++ src/czml3/__init__.py
@@ -1,7 +1,11 @@
 from ._version import get_versions
-from .core import CZML_VERSION, Document, Packet, Preamble
 
 __version__ = get_versions()["version"]
 del get_versions
 
-__all__ = ["Document", "Preamble", "Packet", "CZML_VERSION"]
+try:
+    from .core import CZML_VERSION, Document, Packet, Preamble
+
+    __all__ = ["Document", "Preamble", "Packet", "CZML_VERSION"]
+except Exception:
+    pass  # required for dynamic versioning using setuptools in pyproject.toml
