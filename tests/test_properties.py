0a034b01936a4c849d5636ed53612036ed86be34
Daniel Stoops <dainelst@omnisys.co.il>
2024-11-26 14:22:25 +0200

    Add test_material_image_uri()

--- tests/test_properties.py
+++ tests/test_properties.py
@@ -431,6 +431,37 @@ def test_material_image():
     )
     assert str(mat) == expected_result
 
+
+def test_material_image_uri():
+    expected_result = """{
+    "image": {
+        "image": "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7",
+        "repeat": [
+            2,
+            2
+        ],
+        "color": {
+            "rgba": [
+                200,
+                100,
+                30,
+                255
+            ]
+        }
+    }
+}"""
+
+    mat = Material(
+        image=ImageMaterial(
+            image=Uri(
+                uri="""data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"""
+            ),
+            repeat=[2, 2],
+            color=Color(rgba=[200, 100, 30]),
+        )
+    )
+    assert str(mat) == expected_result
+
     pol_mat = PolylineMaterial(
         image=ImageMaterial(
             image=Uri(uri="https://site.com/image.png"),
